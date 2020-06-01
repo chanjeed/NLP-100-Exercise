a8c1b463af82768c999a38bd31f1507be00b836e
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2020-06-02 07:20:19 +0900

    problem 05

--- Chapter1/05.py
+++ Chapter1/05.py
@@ -1,3 +1,10 @@
-sentence = “I am an NLPer”
-words = list(sentence.split())
+sentence = "I am an NLPer"
+
+def n_gram(string,n=2):
+    
+    return [string[idx:idx+n] for idx in range(len(string) - n +1)]
+
+print(n_gram(sentence,2))
+print(n_gram(sentence.split(' '),2))
+
 
